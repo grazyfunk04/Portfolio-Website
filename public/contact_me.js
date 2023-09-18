32f75fdefd5261d3fc5ee43e47e9270f0804668b
grazyfunk04 <123224623+grazyfunk04@users.noreply.github.com>
2023-09-18 15:29:27 +0530

    Add files via upload

--- public/contact_me.js
+++ public/contact_me.js
@@ -33,18 +33,3 @@ icon.onclick = function(){
     }
 }
 
-function sendEmail(){
-    Email.send({
-        Host : "smtp.gmail.com",
-        Username : "kunalmehndi10@gmail.com",
-        Password : "",
-        To : 'kunalmehndi10@gmail.com',
-        From : document.getElementById("email").value,
-        Subject : "New Contact Form",
-        Body : "Name: " + document.getElementById("name").value + 
-        "<br> Email: " + document.getElementById("email").value + 
-        "<br> Message: " + document.getElementById("message").value,
-    }).then(
-      message => alert("Message Sent Successfully")
-    );
-}
